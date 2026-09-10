70316e93067e881cf2890ce7bef82431612dc4a2
pkumar2026 <parmod.kumar@pattern.com>
2026-09-10 15:39:06 -0600

    feat: bump dox to 0.2.1

--- Formula/dox.rb
+++ Formula/dox.rb
@@ -1,15 +1,15 @@
 class Dox < Formula
   desc "Focused Docker TUI for colima -- lazydocker, but smaller"
   homepage "https://github.com/pkumar2026/dox"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   if Hardware::CPU.arm?
     url "https://github.com/pkumar2026/dox/releases/download/v#{version}/dox-v#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "c41805da4417a9bec675e73b7eb1e00365ca91a77e33c753fe9b72a35c7e0689"
+    sha256 "d186d27c3db6cafff8ac4117083c52b87836fbc7f529c3cdcdcbb81805551251"
   else
     url "https://github.com/pkumar2026/dox/releases/download/v#{version}/dox-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "d4bee2565ad7e9703bc8184317435ebd1892887a3c8eb89a4839bc54b1c103a7"
+    sha256 "6bd8ca6b6e33af6f85ed9bea0b913feb0b0462dc234cdd89a0dd0d7ba5620a73"
   end
 
   def install
